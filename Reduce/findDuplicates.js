c935a2dcdf12619cb3f8daf74fce971ad91a7d63
John Binning <john.binning17@gmail.com>
2017-09-01 13:46:13 -0600

    add findDuplicates to reduce

---
+++ Reduce/findDuplicates.js
@@ -0,0 +1,19 @@
+
+
+const findDuplicates = (nums) => {
+    let dups = nums.reduce((acc, num) => {
+        acc[num] ? acc[num] += 1 : acc[num] = 1;
+        return acc;
+    },{})
+    return Object.keys(dups)
+    .map( numb => {
+      return parseInt(numb);
+    })
+    .filter( num => {
+      return parseInt(dups[num]) > 1;
+    });
+};
+
+findDuplicates([4,3,2,7,8,2,3,1])
+
+// => [2, 3]
