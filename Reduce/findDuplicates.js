8a1574ba09d16c71881bb686c2c4594e34e3360b
John Binning <john.binning17@gmail.com>
2017-09-01 13:47:34 -0600

    add findDuplicates explaination

--- Reduce/findDuplicates.js
+++ Reduce/findDuplicates.js
@@ -1,4 +1,10 @@
+//Find all the elements that appear twice in this array.
 
+// Input:
+// [4,3,2,7,8,2,3,1]
+
+// Output:
+// [2,3]
 
 const findDuplicates = (nums) => {
     let dups = nums.reduce((acc, num) => {
