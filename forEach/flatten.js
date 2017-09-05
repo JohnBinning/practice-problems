9c4329de65704fdd63ba09e7dcc911605303559d
John Binning <john.binning17@gmail.com>
2017-09-05 13:27:09 -0600

    update flatten with examples

--- forEach/flatten.js
+++ forEach/flatten.js
@@ -1,21 +1,22 @@
 //write a function that flattens nested arrays
 
-// flatten([[1,2,[3]],4]);
-
-// => [1, 2, 3, 4]
+// flatten([[1,2,[3]],4])  => [1, 2, 3, 4]
+//and [[1,2,3],4] => [1, 2, 3, 4]
+// and [1, [2,[3, 4, [5], 6]]] => [1, 2, 3, 4, 5, 6]
 
 const flatten = (notFlat) => {
   const flattened = [];
 
   const flattener = (toBeFlattened) => {
-    toBeFlattened.forEach( val => {
+    toBeFlattened.forEach( (val) => {
       if(!Array.isArray(val)) {
         flattened.push(val);
       } else {
         flattener(val);
       }
     })
-  }
+  };
+
   flattener(notFlat);
   return flattened;
 };
