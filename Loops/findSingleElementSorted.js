680ad6fc7ab04e9b6957a746c98789b5635c53a9
John Binning <john.binning17@gmail.com>
2017-09-05 14:13:47 -0600

    fix find single element example spacing

--- Loops/findSingleElementSorted.js
+++ Loops/findSingleElementSorted.js
@@ -2,10 +2,10 @@
 // except for one element which appears once. Find this single element that appears only once.
 
 // Example 1:
-// Input: [1,1,2,3,3,4,4,8,8]
+// Input: [1, 1, 2, 3, 3, 4, 4, 8, 8]
 // Output: 2
 // Example 2:
-// Input: [3,3,7,7,10,11,11]
+// Input: [3, 3, 7, 7, 10, 11, 11]
 // Output: 10
 
 const singleNonDuplicate = (nums) => {
