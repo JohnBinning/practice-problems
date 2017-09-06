13164616eaef6bb50d07f3fe089b57e204c5a3ab
John Binning <john.binning17@gmail.com>
2017-09-06 13:16:33 -0600

    add singleNumber directions

--- Reduce/findSingle.js
+++ Reduce/findSingle.js
@@ -1,14 +1,14 @@
-
+//Given an array of integers, every element appears twice except for one. Find that single one.
 
 const singleNumber = (nums) => {
-    let dups = nums.reduce((acc, num) => {
-        acc[num] ? acc[num] += 1 : acc[num] = 1;
-        return acc;
-    },{});
+  let dups = nums.reduce((acc, num) => {
+    acc[num] ? acc[num] += 1 : acc[num] = 1;
+    return acc;
+  },{});
 
-    const numKeys = Object.keys(dups).map( numb => parseInt(numb) );
-    const singleNum = numKeys.find( num => dups[num] === 1 );
-    return singleNum;
+  const numKeys = Object.keys(dups).map( numb => parseInt(numb) );
+  const singleNum = numKeys.find( num => dups[num] === 1 );
+  return singleNum;
 };
 
 singleNumber([1,1,2,3,3,4,4]);
