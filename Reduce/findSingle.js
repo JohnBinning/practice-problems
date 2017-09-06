c602c4db758e4d4539879429f2e353640e5d6a6e
John Binning <john.binning17@gmail.com>
2017-09-06 13:14:50 -0600

    add singleNumber

---
+++ Reduce/findSingle.js
@@ -0,0 +1,16 @@
+
+
+const singleNumber = (nums) => {
+    let dups = nums.reduce((acc, num) => {
+        acc[num] ? acc[num] += 1 : acc[num] = 1;
+        return acc;
+    },{});
+
+    const numKeys = Object.keys(dups).map( numb => parseInt(numb) );
+    const singleNum = numKeys.find( num => dups[num] === 1 );
+    return singleNum;
+};
+
+singleNumber([1,1,2,3,3,4,4]);
+
+// => 2
