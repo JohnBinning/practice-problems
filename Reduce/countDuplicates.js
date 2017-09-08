143554f89d55e846ddfcfff181309340e3fe1f3c
John Binning <john.binning17@gmail.com>
2017-09-08 14:30:33 -0600

    add countDuplicates examples

--- Reduce/countDuplicates.js
+++ Reduce/countDuplicates.js
@@ -3,7 +3,6 @@
 // The input string can be assumed to contain only alphabets 
 // (both uppercase and lowercase) and numeric digits.
 
-
 const duplicateCount = (text) => {
   let dups = text.toLowerCase().split('').reduce((acc, num) => {
         acc[num] ? acc[num] += 1 : acc[num] = 1;
@@ -14,3 +13,10 @@ const duplicateCount = (text) => {
     const singleDupes = numKeys.filter( num => dups[num] > 1 );
     return singleDupes.length;
 }
+
+// Test.assertEquals(duplicateCount(""), 0);
+// Test.assertEquals(duplicateCount("abcde"), 0);
+// Test.assertEquals(duplicateCount("aabbcde"), 2);
+// Test.assertEquals(duplicateCount("aabBcde"), 2,"should ignore case");
+// Test.assertEquals(duplicateCount("Indivisibility"), 1)
+// Test.assertEquals(duplicateCount("Indivisibilities"), 2, "characters may not be adjacent")
