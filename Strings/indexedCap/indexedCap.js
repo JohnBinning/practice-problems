0071c4ec271a0c8e2d31e4ebff8b85377fb95bc6
John Binning <john.binning17@gmail.com>
2017-10-02 02:36:03 -0600

    add indexedCap

---
+++ Strings/indexedCap/indexedCap.js
@@ -0,0 +1,12 @@
+const capitalize = (s,arr) => {
+  return s.split('').map( (letter, i) => {
+    return arr.includes(i) ? letter.toUpperCase() : letter;
+  }).join('');
+};
+
+Test.assertDeepEquals(capitalize("abcdef",[1,2,5]),'aBCdeF');
+Test.assertDeepEquals(capitalize("abcdef",[1,2,5,100]),'aBCdeF');
+Test.assertDeepEquals(capitalize("codewars",[1,3,5,50]),'cOdEwArs');
+Test.assertDeepEquals(capitalize("abracadabra",[2,6,9,10]),'abRacaDabRA');
+Test.assertDeepEquals(capitalize("codewarriors",[5]),'codewArriors');
+Test.assertDeepEquals(capitalize("indexinglessons",[0]),'Indexinglessons');
