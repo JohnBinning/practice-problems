4fefb2bf4c694749ea0f04682c5f557d9b0746a7
John Binning <john.binning17@gmail.com>
2017-11-01 00:48:41 -0600

    update nicknames w/ additional solution

--- Ruby/Slice/nicknames/nicknames.rb
+++ Ruby/Slice/nicknames/nicknames.rb
@@ -4,3 +4,15 @@ def nickname_generator(name)
   return name.chars.slice(0, 3).join('') unless (vowels.include?(name[2]))
   name.chars.slice(0, 4).join('')
 end
+
+# or
+
+def nickname_generator(name)
+  return "Error: Name too short" if name.size < 4
+
+  if name[2] =~ /[aeiou]/
+    name[0..3]
+  else
+    name[0..2]
+  end
+end
