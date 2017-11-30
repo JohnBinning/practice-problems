d453ae6d32f00dfe7edbcdcb3d006976400f0584
John Binning <john.binning17@gmail.com>
2017-11-29 18:21:09 -0700

    update primeFactors

--- Javascript/Math/primeFactors/primeFactors.js
+++ Javascript/Math/primeFactors/primeFactors.js
@@ -16,3 +16,19 @@ const getPrimeFactors = (num) => {
 };
 
 getPrimeFactors(102); // => [ 2, 3, 17 ]
+
+
+function colorProbability(color, texture){
+  const bag = [
+    { color: 'red', texture: 'smooth', quantity: 1},
+    { color: 'red', texture: 'bumpy', quantity: 4},
+    { color: 'yellow', texture: 'bumpy', quantity: 2},
+    { color: 'yellow', texture: 'smooth', quantity: 1},
+    { color: 'green', texture: 'bumpy', quantity: 1},
+    { color: 'green', texture: 'smooth', quantity: 1}
+  ];
+  const totalBumpy = 6, totalSmooth = 4;
+  return bag.filter(obj => {
+    return obj.texture === 'bumpy';
+  })
+}
