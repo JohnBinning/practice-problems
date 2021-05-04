ab177da37aa59fabe208c4745f679bf029dabc07
John Binning <john.binning17@gmail.com>
2021-05-04 10:32:04 -0600

    add repeatedDNA

---
+++ Javascript/Sets/repeatedDNA/repeatedDNA.ts
@@ -0,0 +1,35 @@
+function findRepeatedDnaSequences(s: string): string[] {
+  const dnaStore = new Set<string>(), result = new Set<string>();
+  for (let i = 0; i < s.length - 9; i++) {
+      const sub = s.substr(i, 10);
+      if (dnaStore.has(sub)) {
+          result.add(sub);
+      } else {
+          dnaStore.add(sub);
+      }
+  }
+  return Array.from(result);
+};
+
+
+/*
+
+The DNA sequence is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T'.
+
+For example, "ACGAATTCCG" is a DNA sequence.
+When studying DNA, it is useful to identify repeated sequences within the DNA.
+
+Given a string s that represents a DNA sequence, return all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+Output: ["AAAAACCCCC","CCCCCAAAAA"]
+Example 2:
+
+Input: s = "AAAAAAAAAAAAA"
+Output: ["AAAAAAAAAA"]
+
+*/
